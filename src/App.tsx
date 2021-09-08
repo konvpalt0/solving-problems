1027350c264059cfa85613b6922b5eef1eff2f39
konvpalto <www.konvpalto@yandex.ru>
2021-09-08 16:32:00 +0500

    Added basic navigation between problems by using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import TwoSum from "./problem-sets/easy/two-sum/TwoSum";
-
+import ReverseInteger from "./problem-sets/easy/reverse-integer/ReverseInteger";
+import {NavLink, Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <TwoSum/>
+      <div>
+        <div>
+          <NavLink to={"/reverse-integer"}>Reverse Integer</NavLink>
+          <br/>
+          <NavLink to={"/two-sum"}>Two Sum</NavLink>
+        </div>
+      </div>
+      <Route path={"/reverse-integer"}>
+        <ReverseInteger/>
+      </Route>
+      <Route path={"/two-sum"}>
+        <TwoSum/>
+      </Route>
     </div>
   );
 }
